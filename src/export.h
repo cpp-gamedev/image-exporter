5114e9a76c90a14871ada881521435283ffec3f9
wwsmiff <63499687+wwsmiff@users.noreply.github.com>
2023-05-04 19:50:58 +0530

    Clean up code (#9)
    * Clean up code
    
    * Add suggested changes
    
    * fix minor typo
    
    * remove unused file: <fstream.h> from export.h

--- src/export.h
+++ src/export.h
@@ -1,15 +1,23 @@
 #pragma once
 
 #include <vector>
-#include <span>
 #include <cstdint>
+#include <string>
 
 using std::uint8_t;
 using std::uint32_t;
 
 struct Rgba
 {
+	Rgba(uint32_t hex);
+	Rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 	uint8_t r{0}, g{0}, b{0}, a{0};
+	static constexpr uint32_t to_hex(const Rgba &rgba);
+};
+
+struct Index2D
+{
+	uint32_t x{0}, y{0};
 };
 
 struct Extent
@@ -19,8 +27,13 @@ struct Extent
 
 struct Image
 {
-	std::vector<Rgba> pixels;
+	Image(uint32_t width, uint32_t height);
+	static void export_to_ppm(const Image &input, const std::string &path);
+	Rgba &operator[](Index2D index);
+	const Rgba &operator[](Index2D index) const;
+	
 	Extent extent;
+	std::vector<Rgba> pixels;
 };
 
-Image upscale(std::span<Rgba const> input, uint32_t scale);
+Image upscale(const Image &input, uint32_t scale);

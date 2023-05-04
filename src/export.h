b6e80a78bfad96ce28a05169218106a040458f07
wwsmiff <arnav0872@gmail.com>
2023-05-04 18:56:52 +0530

    Clean up code

--- src/export.h
+++ src/export.h
@@ -1,15 +1,27 @@
 #pragma once
 
+#include <array>
 #include <vector>
 #include <span>
 #include <cstdint>
+#include <string>
+#include <fstream>
 
 using std::uint8_t;
 using std::uint32_t;
 
 struct Rgba
 {
+	Rgba(uint32_t hex);
+	Rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+	~Rgba();
 	uint8_t r{0}, g{0}, b{0}, a{0};
+	static constexpr uint32_t to_hex(const Rgba &rgba);
+};
+
+struct Index2D
+{
+	uint32_t x{0}, y{0};
 };
 
 struct Extent
@@ -19,8 +31,14 @@ struct Extent
 
 struct Image
 {
-	std::vector<Rgba> pixels;
+	Image(uint32_t width, uint32_t height);
+	~Image();
+	static void export_to_ppm(const Image &input, const std::string &path);
+	Rgba &operator[](Index2D index);
+	const Rgba &operator[](Index2D index) const;
+	
 	Extent extent;
+	std::vector<Rgba> pixels;
 };
 
-Image upscale(std::span<Rgba const> input, uint32_t scale);
+Image upscale(const Image &input, uint32_t scale);
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
 #include "export.h"
 
-constexpr uint32_t image_width_v = 512;
-constexpr uint32_t image_height_v = 512;
-constexpr uint32_t input_extent_v = 1;
-constexpr uint32_t output_extent_v = 32;
-
 int main()
 {
 	return 0;
